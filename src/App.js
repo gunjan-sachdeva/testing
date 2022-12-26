6ad0ca941c5c177effca7f39a2f826d8baa91891
gunjan-sachdeva <58691180+gunjan-sachdeva@users.noreply.github.com>
2022-12-26 12:17:04 +0530

    Contact

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>welcome to the world of React Js</h1>
       <Home />
       <About />
+      <Contact />
     </div>
   );
 }
