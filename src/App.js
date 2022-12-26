9eb78e75d10608930d359d41b410e3f6759a1cf8
gunjan-sachdeva <58691180+gunjan-sachdeva@users.noreply.github.com>
2022-12-26 12:31:11 +0530

    dev

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
       <Home />
       <About />
       <Contact />
+
+      <Gallery />
     </div>
   );
 }
