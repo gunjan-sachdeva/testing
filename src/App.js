1f1fe7dfd4e8f990b63c524309a40aa233309411
gunjan-sachdeva <58691180+gunjan-sachdeva@users.noreply.github.com>
2022-12-26 12:12:01 +0530

    About us

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <h1>welcome to the world of React Js</h1>
       <Home />
+      <About />
     </div>
   );
 }
